553f2ac4b24e57cf744e8d0973bff032c3b0ef34
Fadhli Syaifullah <me@fadhli.dev>
2023-06-15 23:01:15 +0700

    ADD: validators utility

--- src/main/kotlin/com/alunud/application/Application.kt
+++ src/main/kotlin/com/alunud/application/Application.kt
@@ -1,11 +1,21 @@
 package com.alunud.application
 
+import com.alunud.util.Validators
+import jakarta.validation.Validator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-class AlUnudApplication
+class AlUnudApplication {
+
+    @Bean
+    fun validators(validator: Validator): Validators {
+        return Validators(validator)
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<AlUnudApplication>(*args)
---
+++ src/main/kotlin/com/alunud/util/Validators.kt
@@ -0,0 +1,16 @@
+package com.alunud.util
+
+import jakarta.validation.ConstraintViolationException
+import jakarta.validation.Validator
+
+class Validators(private val validator: Validator) {
+
+    fun validate(any: Any) {
+        val result = validator.validate(any)
+
+        if (result.size != 0) {
+            throw ConstraintViolationException(result)
+        }
+    }
+
+}

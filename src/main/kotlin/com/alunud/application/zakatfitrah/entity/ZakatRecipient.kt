4de3a988b92f73523c904b0d6c2acd891337b84c
Fadhli Syaifullah <me@fadhli.dev>
2023-06-15 11:06:47 +0700

    ADD: zakat fitrah recipient entity

---
+++ src/main/kotlin/com/alunud/application/zakatfitrah/entity/ZakatRecipient.kt
@@ -0,0 +1,24 @@
+package com.alunud.application.zakatfitrah.entity
+
+import jakarta.persistence.*
+import jakarta.persistence.FetchType.LAZY
+import java.util.*
+
+@Entity
+@Table(name = "zakat_fitrah_recipients")
+data class ZakatRecipient(
+    @field:Id
+    val id: UUID,
+    val name: String,
+
+    @field:Column(nullable = true)
+    val address: String?,
+
+    @field:Column(nullable = true)
+    val givenTime: Long?,
+    val givenAmount: Double,
+
+    @field:ManyToOne(fetch = LAZY)
+    @field:JoinColumn(name = "zakat_id")
+    val zakatEdition: ZakatEdition
+)

1dbb7f28ef1eb0ea4b26f62da1b25ea564cc0f53
Fadhli Syaifullah <me@fadhli.dev>
2023-06-15 11:08:11 +0700

    ADD: zakat fitrah edition repository

---
+++ src/main/kotlin/com/alunud/application/zakatfitrah/repository/ZakatEditionRepository.kt
@@ -0,0 +1,9 @@
+package com.alunud.application.zakatfitrah.repository
+
+import com.alunud.application.zakatfitrah.entity.ZakatEdition
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface ZakatEditionRepository : JpaRepository<ZakatEdition, UUID>

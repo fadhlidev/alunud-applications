5dfbbbf6d3f2eb580e0f5081205bd42646f3e7ab
Fadhli Syaifullah <me@fadhli.dev>
2023-06-24 00:35:23 +0700

    ADD: BCrypt password encoder

--- src/main/kotlin/com/alunud/application/Application.kt
+++ src/main/kotlin/com/alunud/application/Application.kt
@@ -6,12 +6,17 @@ import jakarta.validation.Validator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 class AlUnudApplication {
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 
     @Bean
     fun validators(validator: Validator): Validators {

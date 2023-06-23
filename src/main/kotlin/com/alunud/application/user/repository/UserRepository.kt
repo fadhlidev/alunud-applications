d0fac890e4d5f681adda08b19cb3a482c20ea018
Fadhli Syaifullah <me@fadhli.dev>
2023-06-24 06:46:08 +0700

    ADD: User repository

---
+++ src/main/kotlin/com/alunud/application/user/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.alunud.application.user.repository
+
+import com.alunud.application.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface UserRepository : JpaRepository<User, UUID> {
+    fun findByUsername(username: String): User?
+}

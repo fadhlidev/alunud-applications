eb16d7420b12406ac8296aa8812a54bd96a19ad3
Fadhli Syaifullah <me@fadhli.dev>
2023-06-24 10:23:46 +0700

    ADD: redis service interface

---
+++ src/main/kotlin/com/alunud/application/persistence/memory/service/RedisService.kt
@@ -0,0 +1,9 @@
+package com.alunud.application.persistence.memory.service
+
+interface RedisService {
+    suspend fun setValue(key: String, value: String)
+    suspend fun setValue(key: String, value: String, exp: ULong)
+    suspend fun getValue(key: String): String?
+    suspend fun deleteValue(key: String)
+    suspend fun deleteAllValues()
+}

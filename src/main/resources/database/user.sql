b8cec1a3f9d80e06fd3d43443d6853d85048a3d1
Fadhli Syaifullah <me@fadhli.dev>
2023-06-25 14:12:44 +0700

    EDIT: add users roles DDL

--- src/main/resources/database/user.sql
+++ src/main/resources/database/user.sql
@@ -1,7 +1,24 @@
 CREATE TABLE users
 (
-    id       VARCHAR(50) PRIMARY KEY,
+    id       UUID         NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL UNIQUE,
     email    VARCHAR(255),
-    password VARCHAR(500) NOT NULL
+    password VARCHAR(500) NOT NULL,
+    PRIMARY KEY (id, username)
+);
+
+CREATE TABLE roles
+(
+    id   UUID         NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (id, name)
+);
+
+CREATE TABLE users_roles
+(
+    user_username VARCHAR(255) NOT NULL,
+    role_name     VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_username) REFERENCES users (username),
+    FOREIGN KEY (role_name) REFERENCES roles (name),
+    PRIMARY KEY (user_username, role_name)
 )

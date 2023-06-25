5bc175115c9d67abad5fdfd8a687558d9450fcd7
Fadhli Syaifullah <me@fadhli.dev>
2023-06-25 15:58:10 +0700

    EDIT: add authentication to the security config

--- src/main/kotlin/com/alunud/application/config/SecurityConfig.kt
+++ src/main/kotlin/com/alunud/application/config/SecurityConfig.kt
@@ -1,27 +1,36 @@
 package com.alunud.application.config
 
+import com.alunud.application.auth.filter.TokenAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun passwordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun securityFilterChain(
+        http: HttpSecurity,
+        tokenAuthenticationFilter: TokenAuthenticationFilter
+    ): SecurityFilterChain {
         return http
-            .csrf {
-                it.disable()
-            }
-            .cors {
-                it.disable()
-            }
+            .csrf { it.disable() }
+            .cors { it.disable() }
             .authorizeHttpRequests {
+                it.requestMatchers("/api/**").authenticated()
                 it.anyRequest().permitAll()
             }
+            .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
 
--- src/main/kotlin/com/alunud/application/config/ValidatorConfig.kt
+++ src/main/kotlin/com/alunud/application/config/ValidatorConfig.kt
@@ -6,16 +6,10 @@ import jakarta.validation.Validator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
 @EnableAspectJAutoProxy
 class ValidatorConfig {
-
-    @Bean
-    fun passwordEncoder(): BCryptPasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
 
     @Bean
     fun validators(validator: Validator): Validators {

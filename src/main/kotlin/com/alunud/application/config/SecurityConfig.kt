8b88999d2c9f5e248e99f4288df39f31a592f454
Fadhli Syaifullah <me@fadhli.dev>
2023-06-30 21:27:25 +0700

    EDIT: add secured config and access denied handler

--- src/main/kotlin/com/alunud/application/config/SecurityConfig.kt
+++ src/main/kotlin/com/alunud/application/config/SecurityConfig.kt
@@ -1,8 +1,13 @@
 package com.alunud.application.config
 
 import com.alunud.application.auth.filter.TokenAuthenticationFilter
+import com.alunud.web.JsonResponse
+import com.google.gson.Gson
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -11,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 class SecurityConfig {
 
     @Bean
@@ -26,6 +32,19 @@ class SecurityConfig {
         return http
             .csrf { it.disable() }
             .cors { it.disable() }
+            .exceptionHandling {
+                it.accessDeniedHandler { _, response, _ ->
+                    val jsonResponse = JsonResponse(
+                        status = HttpStatus.FORBIDDEN.value(),
+                        message = "FORBIDDEN_REQUEST",
+                        error = "Could not access resource"
+                    )
+
+                    response.contentType = MediaType.APPLICATION_JSON_VALUE
+                    response.status = HttpStatus.FORBIDDEN.value()
+                    response.writer.write(Gson().toJson(jsonResponse))
+                }
+            }
             .authorizeHttpRequests { it.anyRequest().permitAll() }
             .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
